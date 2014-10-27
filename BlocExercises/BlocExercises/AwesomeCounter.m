8a8ff498432d20642b8b6558596ed62e6007c1c5
Andrew Carvajal <andrewcarvajal1@gmail.com>
2014-10-27 18:36:35 -0400

    Optimizations to AwesomeCounter

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -13,15 +13,17 @@
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
     
+    /* Create a mutable string */
     NSMutableString *numberString = [NSMutableString string];
     
+    /* Switch number and otherNumber if number is bigger */
     if (number > otherNumber) {
         NSInteger tempNumber = number;
         number = otherNumber;
         otherNumber = tempNumber;
     }
     
-//    while (number <= otherNumber) {
+    /* Increment number and append to numberString while it is smaller than otherNumber */
     for (NSInteger i = number; i <= otherNumber; i++) {
         [numberString appendString:@(number).stringValue];
         number++;
